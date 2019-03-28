0ed821c16cc0898f64961241c8c272e20ad8f81d
KraynovAlexander <kraynovsasha@gmail.com>
2019-03-28 18:28:56 +0300

    added Cmake
    added necessary Cmake files

---
+++ cmake-build-debug/CMakeFiles/SomeFinishedKatas.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SomeFinishedKatas.dir/screen_lock_patterns.cpp.obj"
+  "CMakeFiles/SomeFinishedKatas.dir/heap_dijkstra_impl.cpp.obj"
+  "SomeFinishedKatas.pdb"
+  "SomeFinishedKatas.exe"
+  "SomeFinishedKatas.exe.manifest"
+  "libSomeFinishedKatas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SomeFinishedKatas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
